212ee17be2bd8c7d657bf13ef0e0e5fa30b47ad1
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-06-20 22:43:44 -0600

    TRV-10: Style FlightInfo Component

--- src/components/FlightInfo/index.tsx
+++ src/components/FlightInfo/index.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react";
 import type { Flight } from "../../models/Flight";
+import styles from "./styles.module.scss";
 
 interface FlightInfoProps {
   flight: Flight;
@@ -7,33 +8,17 @@ interface FlightInfoProps {
 
 const FlightInfo: FC<FlightInfoProps> = ({ flight }) => {
   return (
-    <div className="flight-info">
-      <h2>Flight Information</h2>
-      <div className="flight-details">
-        <div className="flight-origin">
-          <h3>Origin</h3>
-          <p>
-            Airport Code: {flight.origin.airportCode}
-            <br />
-            Time Zone: {flight.origin.timeZone}
-            <br />
-            Date: {new Date(flight.origin.dateTime).toLocaleDateString()}
-            <br />
-            Time: {new Date(flight.origin.dateTime).toLocaleTimeString()}
-          </p>
-        </div>
-        <div className="flight-destination">
-          <h3>Destination</h3>
-          <p>
-            Airport Code: {flight.destination.airportCode}
-            <br />
-            Time Zone: {flight.destination.timeZone}
-            <br />
-            Date: {new Date(flight.destination.dateTime).toLocaleDateString()}
-            <br />
-            Time: {new Date(flight.destination.dateTime).toLocaleTimeString()}
-          </p>
-        </div>
+    <div className={styles.flightInfo}>
+      <div className={styles.flightDetails}>
+        <h2>{flight.origin.airportCode}</h2>
+        <p>{new Date(flight.origin.dateTime).toLocaleDateString()}</p>
+        <p>{new Date(flight.origin.dateTime).toLocaleTimeString()}</p>
+      </div>
+      <h2>✈️</h2>
+      <div className={styles.flightDetails}>
+        <h2>{flight.destination.airportCode}</h2>
+        <p>{new Date(flight.destination.dateTime).toLocaleDateString()}</p>
+        <p>{new Date(flight.destination.dateTime).toLocaleTimeString()}</p>
       </div>
     </div>
   );
